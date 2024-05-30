de93ceaf177f7446428aa7171a18e2ad245a83df
Damian Kolacki <dasmixovski@gmail.com>
2024-05-30 19:07:13 +0000

    Add ship placement logic to initialize random ship coordinates

--- run.py
+++ run.py
@@ -2,16 +2,28 @@ import random
 
 class Board:
 
-
     def __init__(self, size, ships_num):
+        
         self.size = size
-        self.board = [["[ ]" for x in range(size)] for y in range(size)]
+        self.board = [["[~]" for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
+        self.guesses = []
+        self.ships = []
+
+    def place_ships(self):
+    
+        ships = []
+        while len(ships) < self.num_ships:
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
+            if (x, y) not in ships:
+                ships.append((x, y))
+        return ships
         
 
 def new_game():
 
-    size = 5
+    size = 8
     num_ships = 5
     print("." * 35)
     print("Welcome to Battleships Game!")
